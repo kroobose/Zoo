c253039563cbda03ca19290606e271a3659a4cd9
nakajmiya <t221d064@gunma-u.ac.jp>
2023-11-09 22:22:02 +0900

    initial commit

---
+++ training_mode/conventional_training/mobileface_net.sh
@@ -0,0 +1,20 @@
+mkdir 'log'
+python train.py \
+    --data_root '/workspace/dataset/DigiFace-1M/5_images_per_identity' \
+    --train_file '/workspace/dataset/DigiFace-1M/5_images_per_identity/train_list.txt' \
+    --backbone_type 'MobileFaceNet' \
+    --backbone_conf_file '../backbone_conf.yaml' \
+    --head_type 'MagFace' \
+    --head_conf_file '../head_conf.yaml' \
+    --lr 0.1 \
+    --out_dir 'out_dir' \
+    --epoches 50 \
+    --step '10, 13, 16, 20, 25, 40' \
+    --print_freq 200 \
+    --save_freq 3000 \
+    --batch_size 512 \
+    --momentum 0.9 \
+    --log_dir 'log' \
+    --tensorboardx_logdir 'mv-hrnet' \
+    2>&1 | tee log/log.log
+
